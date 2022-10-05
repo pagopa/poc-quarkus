01ec40aea250b63e55cb4e0cb499315aff3d1cfe
FedericoRuzzier <federicoruzzier96@gmail.com>
2022-10-05 17:08:07 +0200

    Fixing swagger and openapi creation, plus readme

--- openapi/README.md
+++ openapi/README.md
@@ -2,4 +2,4 @@
 
 To generate the openapi.json and swagger.json files:
 1. run the server on `localhost:8080`
-2. run from this directory the script `generate_swagger.sh`
+2. run from this directory the script `generate_openapi_and_swagger.sh`
--- src/main/java/it/gov/pagopa/Controller/HomeController.java
+++ src/main/java/it/gov/pagopa/Controller/HomeController.java
@@ -1,14 +1,23 @@
 package it.gov.pagopa.Controller;
 
+import java.net.URI;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import io.quarkus.logging.Log;
 import it.gov.pagopa.Model.AppInfo;
 
 @Produces(value = MediaType.APPLICATION_JSON)
@@ -23,7 +32,17 @@ public class HomeController {
 
     @ConfigProperty(name = "properties.environment")
     private String environment;
+
+    @GET
+    @Path("")
+    public Response home(){
+        return Response.seeOther(UriBuilder.fromUri("/swagger").build()).build();
+    }
     
+    @APIResponses(value = {
+        @APIResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = AppInfo.class))),
+        @APIResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON))
+    })
     @GET
     @Path("/info")
     public Response healthCheck(){
