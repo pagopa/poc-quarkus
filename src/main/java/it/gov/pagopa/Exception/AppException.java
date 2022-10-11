bf910efe4cdb173553f1efbe23722dc2a6350ae5
FedericoRuzzier <federicoruzzier96@gmail.com>
2022-10-11 10:47:20 +0200

    Inserting unit test the API

--- src/main/java/it/gov/pagopa/Exception/AppException.java
+++ src/main/java/it/gov/pagopa/Exception/AppException.java
@@ -1,8 +1,6 @@
 package it.gov.pagopa.Exception;
 
-import java.io.Serializable;
-
-public class AppException extends RuntimeException implements Serializable{
+public class AppException extends RuntimeException{
     
     String title;
 
---
+++ src/test/java/it/gov/pagopa/Controller/EnrollmentControllerTest.java
@@ -0,0 +1,24 @@
+package it.gov.pagopa.Controller;
+
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static io.restassured.RestAssured.given;
+
+import javax.ws.rs.core.MediaType;
+
+@QuarkusTest
+public class EnrollmentControllerTest {
+    
+    @Test
+    public void testCreateOrganization(){
+        given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .when().post("/organizations/1")
+        .then()
+        .contentType(MediaType.APPLICATION_JSON)
+        .statusCode(201);
+    } 
+}
