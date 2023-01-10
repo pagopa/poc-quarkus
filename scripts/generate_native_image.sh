df72efa593f5f6395923d766fb0b49f9ccd9b36f
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-01-10 16:43:55 +0100

    PAGOPA-597 updating readme file

--- scripts/generate_native_image.sh
+++ scripts/generate_native_image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sed -i '' "s/127.0.0.1/host.docker.internal/g" src/main/resources/application.properties
+
+docker build -f src/main/docker/Dockerfile.graalbase -t graalbase .
+
 mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.native.builder-image=graalbase
 
 docker build -f src/main/docker/Dockerfile.native -t quarkus/poc-quarkus-native:native-aarch64 .
+
+sed -i '' "s/host.docker.internal/127.0.0.1/g" src/main/resources/application.properties
