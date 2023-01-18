a1064d5f0eef4fb105de35ab5726de0cd326c687
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-01-18 09:32:31 +0100

    PAGOPA-597 updating properties files

--- scripts/generate_native_image.sh
+++ scripts/generate_native_image.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-sed -i '' "s/127.0.0.1/host.docker.internal/g" src/main/resources/application.properties
-
 docker build -f src/main/docker/Dockerfile.graalbase -t graalbase .
 
-mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.native.builder-image=graalbase
+mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.native.builder-image=graalbase -Dquarkus.profile=docker-native
 
 docker build -f src/main/docker/Dockerfile.native -t quarkus/poc-quarkus-native:native-aarch64 .
-
-sed -i '' "s/host.docker.internal/127.0.0.1/g" src/main/resources/application.properties
