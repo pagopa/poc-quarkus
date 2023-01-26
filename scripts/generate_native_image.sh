ab0e5582c9b913615b2f0b0d211fec8b29646cde
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-01-26 15:17:56 +0100

    PAGOPA-586 updating files for amd64 architecture

--- scripts/generate_native_image.sh
+++ scripts/generate_native_image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #This script must be run with architecture x86_64
-mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.2-java11
+mvn package -Pnative \
+-Dquarkus.native.container-build=true \
+-Dquarkus.container-image.build=true \
+-Dquarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel-builder-image:22.2-java11
 
-docker build -f src/main/docker/Dockerfile.native -t quarkus/poc-quarkus-native:cloud .
+docker build -f src/main/docker/Dockerfile.native -t quarkus/poc-quarkus-native:native-amd64 .
