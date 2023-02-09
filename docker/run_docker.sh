256d463f6c2227d34082bae8defa41bad6e82fe9
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2023-02-09 09:55:57 +0100

    PAGOPA-585 minor update

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -33,4 +33,4 @@ export containerRegistry=${containerRegistry}
 export image=${image}
 
 stack_name=$(cd .. && basename "$PWD")
-docker-compose -p "${stack_name}" up -d --remove-orphans --force-recreate
+docker-compose -p "${stack_name}" --env-file ./config/.env up -d --remove-orphans --force-recreate
